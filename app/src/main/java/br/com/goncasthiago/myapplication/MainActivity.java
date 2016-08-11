d97fdccd130a6d52023b1806e4823207b1f38d1f
Thiago Debia Goncalves <Thiago Debia Goncalves>
2016-08-11 15:05:47 -0300

    Initial commit

---
+++ app/src/main/java/br/com/goncasthiago/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package br.com.goncasthiago.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.app.Fragment;
+import android.view.ViewGroup;
+
+
+//Activity principal
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    //Activity do fragment
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
